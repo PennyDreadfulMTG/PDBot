15c00f9ab2cf4d38ec887d7002bce3ba937652ff
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-06-17 01:49:57 +1000

    Add more tests about what is or is not considered a token.

--- Tests/TestLogs.cs
+++ Tests/TestLogs.cs
@@ -10,14 +10,14 @@ namespace Tests
 {
     class TestLogs
     {
-        [Test]
-        public void TestLogHandler()
+        [TestCase("skinnersweet1880 is being attacked by [Vampire] token, [Vampire] token, [Vampire Nighthawk], [Vampire Lacerator], and [Lord of Lineage].", 3, 2)]
+        [TestCase("[Eldrazi Scion] token blocks [Lightning Berserker].", 1, 1)]
+        [TestCase("[Elvish Visionary] blocks [Tuktuk the Returned].", 1, 1)]
+        public void TestLogHandler(string line, int cards, int tokens)
         {
-            var line = "skinnersweet1880 is being attacked by [Vampire] token, [Vampire] token, [Vampire Nighthawk], [Vampire Lacerator], and [Lord of Lineage].";
             var parser = new GameLogLine(line);
-            Assert.AreEqual(3, parser.Cards.Count());
-            Assert.AreEqual(2, parser.Tokens.Count());
-
+            Assert.AreEqual(cards, parser.Cards.Count());
+            Assert.AreEqual(tokens, parser.Tokens.Count());
         }
     }
 }
