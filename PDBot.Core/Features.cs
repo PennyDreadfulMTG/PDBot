322c7ce97923c7d8a16e57304fa6a294ff26b47b
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-11-26 10:24:19 +1100

    ughhhhh.  The switcheroo bug is back.

--- PDBot.Core/Features.cs
+++ PDBot.Core/Features.cs
@@ -9,9 +9,9 @@ namespace PDBot.Core
     class Features
     {
         /// <summary>
-        /// There used to be a bug with MTGO that gets the names wrong.
+        /// There is a bug with MTGO that gets the names wrong.
         /// We don't want to tell people incorrect results when this happens.
         /// </summary>
-        public static readonly bool PublishResults = true;
+        public static readonly bool PublishResults = false;
     }
 }
