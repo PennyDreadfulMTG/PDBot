41cf4ae0c599e42c9fd22763e66fa85246e6d515
Katelyn Gigante <clockwork.singularity@gmail.com>
2019-12-29 00:18:55 +1100

    Update TestBuggedCardsApi.cs

--- Tests/TestBuggedCardsApi.cs
+++ Tests/TestBuggedCardsApi.cs
@@ -22,14 +22,5 @@ namespace Tests
             Assert.NotNull(BuggedCards.Bugs.First().Description);
             Assert.NotNull(BuggedCards.Bugs.First().LastConfirmed);
         }
-
-        [Test]
-        public async Task TestVerifications()
-        {
-            var LivingLore = await BuggedCards.GetVerificationForBugAsync(35);
-            var currentBuild = await BuggedCards.GetCurrentBuildAsync();
-            //Assert.AreEqual(LivingLore.Major, 3);
-            //Assert.LessOrEqual(LivingLore, currentBuild);
-        }
     }
 }
