51532e078c5cb58f6f8ef1ec50ba345bc851f71e
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-06-01 20:12:54 +1000

    Gatherling Client is now Seperate

--- Gatherling/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0014:Use ternary operator", Justification = "<Pending>", Scope = "module")]
-
