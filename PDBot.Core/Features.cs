20a6d6cddd583711aa1ac3f34b0edb1a6e557642
Katelyn Gigante <clockwork.singularity@gmail.com>
2021-09-08 13:52:39 +1000

    Disable Voice Channels

--- PDBot.Core/Features.cs
+++ PDBot.Core/Features.cs
@@ -33,7 +33,7 @@ namespace PDBot.Core
             PublishResults = true;
             AnnouncePairings = true;
             JoinGames = true;
-            CreateVoiceChannels = true;
+            CreateVoiceChannels = false;
             ConnectToDiscord = true;
             try
             {
